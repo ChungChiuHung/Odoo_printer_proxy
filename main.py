dd6d3d3c188e02581e231ef41959e855d329cb02
chenghsienyu <chenghsienyu@aim.com>
2022-12-01 17:14:47 +0100

    add and update

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_cors import CORS
+import os
+import win32print
+
+PRINTER_NAME = ''
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/api', methods=['GET'])
+def api():
+    return jsonify({'message': 'Hello World'})
+
+
+@app.route('/api/print', methods=['POST'])
+def api_post_print():
+    data = request.form['printer_data']
+    p = win32print.OpenPrinter(PRINTER_NAME)
+    job = win32print.StartDocPrinter(p, 1, ("test of raw data", None, "RAW"))
+    win32print.StartPagePrinter(p)
+    win32print.WritePrinter(p, data.encode())
+    win32print.EndPagePrinter(p)
+    return jsonify({'status': 'success', 'data': data})
+
+
+'''
+***something need to be done here, for translate html source code to printer data***
+soup = BeautifulSoup(html_input , "html.parser")
+for elem in soup.find_all(["a", "p", "div", "h3", "br"]):
+            elem.replace_with(elem.text + "\n\n")
+'''
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=8080)
