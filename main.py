a2836d8c7651be7248d508410999cccc6430abac
chenghsienyu <chenghsienyu@aim.com>
2022-12-03 23:13:40 +0100

    add html2text by python html parsing

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from flask_cors import CORS
+from html.parser import HTMLParser
 import os
 # import win32print
 
@@ -19,20 +20,23 @@ def api():
 @app.route('/dotmatrix/print', methods=['POST'])
 def api_post_print():
     data = request.form['print_data']
-    # p = win32print.OpenPrinter(PRINTER_NAME)
-    # job = win32print.StartDocPrinter(p, 1, ("test of raw data", None, "RAW"))
-    # win32print.StartPagePrinter(p)
-    # win32print.WritePrinter(p, data.encode())
-    # win32print.EndPagePrinter(p)
-    return jsonify({'status': 'success', 'data': data})
+    f = HTMLFilter()
+    f.feed(data)
+    print(f.text)
+    p = win32print.OpenPrinter(PRINTER_NAME)
+    job = win32print.StartDocPrinter(p, 1, ("test of raw data", None, "RAW"))
+    win32print.StartPagePrinter(p)
+    win32print.WritePrinter(p, f.text.encode('utf8'))
+    win32print.EndPagePrinter(p)
+    return jsonify({'status': 'success', 'data': f.text})
 
 
-'''
-***something need to be done here, for translate html source code to printer data***
-soup = BeautifulSoup(html_input , "html.parser")
-for elem in soup.find_all(["a", "p", "div", "h3", "br"]):
-            elem.replace_with(elem.text + "\n\n")
-'''
+class HTMLFilter(HTMLParser):
+    text = ""
+
+    def handle_data(self, data):
+        self.text += data
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=8080)
